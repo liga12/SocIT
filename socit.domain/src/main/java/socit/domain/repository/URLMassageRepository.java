48b34e9ae14c40e3645dd859f34d2d7b7fcc796a
vzubencko <vladimir.zubencko@gmail.com>
2017-04-26 09:31:05 +0300

    add URLMassageRepository

---
+++ socit.domain/src/main/java/socit/domain/repository/URLMassageRepository.java
@@ -0,0 +1,12 @@
+package socit.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import socit.domain.entity.URLMassage;
+
+public interface URLMassageRepository extends JpaRepository<URLMassage, Integer> {
+
+    @Query("select case when count (u) > 0  then true else false end from URLMassage u where u.url = :url")
+    URLMassage existsByUrl(@Param("url")String url);
+}
