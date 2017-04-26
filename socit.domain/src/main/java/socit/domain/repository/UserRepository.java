a704b7bb8325028a066815076ad47b6959074cb6
vzubencko <vladimir.zubencko@gmail.com>
2017-04-26 09:27:35 +0300

    Add UserRepository

---
+++ socit.domain/src/main/java/socit/domain/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package socit.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import socit.domain.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("select case when count (u) > 0  then true else false end from User u where u.login = :login")
+   Boolean existsByLogin(@Param("login") String login);
+
+    @Query("select case when count (u) > 0  then true else false end from User u where u.email = :email")
+    Boolean existsByEmail(@Param("email") String email);
+}
