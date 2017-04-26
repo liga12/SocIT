8e8e508d9150d4407e399f660a05f4d3e83a9921
vzubencko <vladimir.zubencko@gmail.com>
2017-04-26 12:53:16 +0300

    Add authen controller

--- socit.web/src/main/java/socit/web/controller/AuthenticationController.java
+++ socit.web/src/main/java/socit/web/controller/AuthenticationController.java
@@ -1,4 +1,4 @@
-package socit.web.config.controller;
+package socit.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
